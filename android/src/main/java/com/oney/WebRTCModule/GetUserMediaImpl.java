4260a1c0f5633080f6501cf488fc68f283feb4a7
노원상 <no20190715@douzone.com>
2021-05-17 11:37:22 +0900

    ScreenCapture 소스 변경

--- android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
+++ android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
@@ -379,7 +379,7 @@ class GetUserMediaImpl {
             return null;
         }
 
-        VideoSource videoSource = pcFactory.createVideoSource(videoCapturer.isScreencast());
+        VideoSource videoSource = pcFactory.createVideoSource(false);
         videoCapturer.initialize(surfaceTextureHelper, reactContext, videoSource.getCapturerObserver());
 
         String id = UUID.randomUUID().toString();
